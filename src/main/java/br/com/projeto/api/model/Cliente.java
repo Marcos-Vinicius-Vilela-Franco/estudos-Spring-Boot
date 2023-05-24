4643de19e30196fdc640c4765e9e5b58de619e7a
Marcos-Vinicius-Vilela-Franco <mvmarcos74@gmail.com>
2023-05-24 09:36:42 -0300

    update 5

---
+++ src/main/java/br/com/projeto/api/model/Cliente.java
@@ -0,0 +1,26 @@
+package br.com.projeto.api.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+@Entity
+@Table(name = "clientes")
+@Data
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @NotEmpty(message = "Informe um nome!")
+    private String nome;
+
+    @Email(message = "Informe um email válido!")
+    private String email;
+}
